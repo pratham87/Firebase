f60d0d66d66f09d89c7e628804f64d98822ec067
Prathamesh Kawale <kawa5@live.com>
2017-01-10 19:34:34 -0800

    adding the firebase config

--- sketch.js
+++ sketch.js
@@ -7,13 +7,21 @@ function setup() {
     button = createButton("Press Button");
     button.mousePressed(increment);
     button.position(0, height + 20);
+
+    var config = {
+        apiKey: "AIzaSyCf7ger-2WgkWOYXFDUVJnXKPap0kV4o3o",
+        authDomain: "basicexample-56009.firebaseapp.com",
+        databaseURL: "https://basicexample-56009.firebaseio.com",
+        storageBucket: "basicexample-56009.appspot.com",
+        messagingSenderId: "94795301951"
+    };
+    firebase.initializeApp(config);
+    console.log("Firebase"); //Printing data to console to make sure the code above is executing
 }
 
 function increment() {
     score++;
 }
-
-
 
 function draw() {
     background(0);
